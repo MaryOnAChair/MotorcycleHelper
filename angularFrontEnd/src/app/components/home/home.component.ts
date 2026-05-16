424e5824ef647ebcecea3cca554173c85e140d47
MaryInAChair <WorkMayo@outlook.com>
2026-05-16 17:56:57 -0500

    started mapping backend entity to frontend

--- angularFrontEnd/src/app/components/home/home.component.ts
+++ angularFrontEnd/src/app/components/home/home.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavbarComponent} from '../navbar/navbar.component';
+import {HttpClient} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  motorcyles: any[]=[];
+  motorcyle = {
+    brand: " ",
+    model: " ",
+    year: " "
+  };
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.loadMotorcycles();
+  }
+
+  loadMotorcycles() {
+    this.http.get<any[]>("http://localhost:8080/garage").subscribe((data: any) => {
+      this.motorcyles = data;
+    });
+  }
+
+  addMotorcycle(){
+    this.http.post("http://localhost:8080/garage",
+      this.motorcyle
+    ).subscribe((data: any) => {
+      this.loadMotorcycles();
+    });
+  }
 
 }
