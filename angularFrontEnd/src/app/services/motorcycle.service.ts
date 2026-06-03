00b9613447c9d3e90bf30d4c3abceb2d0dc9a262
MaryInAChair <WorkMayo@outlook.com>
2026-06-02 19:34:17 -0500

    motorcycle service for front end

---
+++ angularFrontEnd/src/app/services/motorcycle.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Motorcycle} from '../models/motorcycle';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MotorcycleService {
+
+  private apiUrl =     'http://localhost:8080/api/motorcycle';
+
+
+
+
+  constructor( private http: HttpClient ) { }
+
+  getAllMotorcycles():
+    Observable<Motorcycle[]> {
+
+    return this.http.get<Motorcycle[]>(
+      this.apiUrl
+    );
+  }
+  createMotorcycle(
+    motorcycle: Motorcycle,
+  ): Observable<Motorcycle> {
+
+    return this.http.post<Motorcycle>(
+      this.apiUrl,
+      motorcycle
+    );
+  }
+
+  deleteMotorcycle(
+    moto_id: number
+  ): Observable<any> {
+    return this.http.delete(
+      `${this.apiUrl}/${moto_id}`,{responseType: 'text'});
+
+  }
+
+
+
+}
