26c464183796e5dd5c28248e1696fe5cdae085e1
MaryInAChair <WorkMayo@outlook.com>
2026-06-25 16:16:29 -0500

    made nav bar only show if logged in + Log Out feature

--- angularFrontEnd/src/app/components/navbar/navbar.component.ts
+++ angularFrontEnd/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {NgIf} from '@angular/common';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,6 +10,14 @@ import {NgIf} from '@angular/common';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+    constructor(private authService: AuthService,private router: Router) { }
+  checkLogin() {
+    return this.authService.getLoginStatus();
+  }
+  logOut() {
+    console.log('Logged out pressed!');
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 
 }
--- angularFrontEnd/src/app/services/auth.service.ts
+++ angularFrontEnd/src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../models/user';
+import {isPlatformBrowser} from '@angular/common';
+import {Inject,PLATFORM_ID} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,13 @@ export class AuthService {
 
   private apiUrl = 'http://localhost:8080/auth';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId:Object) { }
 
+  loginStatus : boolean | undefined;
+
+  isBrowser():boolean {
+    return isPlatformBrowser(this.platformId);
+  }
 
   login(username: string,
         password: string) {
@@ -27,18 +34,29 @@ export class AuthService {
     );
   }
 
+  getLoginStatus() {
+    return this.loginStatus;
+  }
 
 
   register(user:User){
+
     localStorage.clear();
     return this.http.post(`${this.apiUrl}/register`,user);
   }
 
   saveToken(token: string):void{
-    localStorage.setItem('token', token);
+    if(this.isBrowser()) {
+      localStorage.setItem('token', token);
+      this.loginStatus = true;
+    }
   }
 
-
+  logout() {
+    if(this.isBrowser()) {
+      localStorage.removeItem('token');
+      this.loginStatus = false;
+    }}
 
 
 }
